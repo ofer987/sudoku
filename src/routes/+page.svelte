36a438c0ff5da9b4c249bfa1ebe273e891eac7c3
Dan Jakob Ofer <dan@ofer.to>
2024-01-28 19:15:48 -0500

    display sudoku board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
+<script lang="ts">
+	import { generateSudokuPuzzle } from './puzzle';
+	const puzzle = generateSudokuPuzzle();
+</script>
+
+<svelte:head>
+	<title>Dan's Sudoku (Puzzles generated using third-party library)</title>
+</svelte:head>
+
+<div class="puzzle">
+	{#await puzzle then resolvedPuzzle}
+		{#each resolvedPuzzle.answer as value}
+			<div class="tile">{value}</div>
+		{/each}
+	{/await}
+</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	.puzzle {
+		font-family: monospace;
+
+		display: grid;
+		grid-template-rows: 2em 2em 2em 2em 2em 2em 2em 2em 2em;
+		grid-template-columns: 2em 2em 2em 2em 2em 2em 2em 2em 2em;
+		align-items: center;
+		justify-items: center;
+	}
+</style>
