91d87c6cacdec4a7e0a44e867ba33ad8bf220a26
Dan Jakob Ofer <dan@ofer.to>
2024-02-11 09:22:59 -0500

    chore(title): title is sudoku

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
 	import { generatePuzzleFromBase64Hash, generateSudokuPuzzle } from './puzzle';
 	import Puzzle from './Puzzle.svelte';
+	import { page } from '$app/stores';
 
 	let puzzle: Puzzle | undefined;
-	// let awaitedPuzzle: Promise<Puzzle> | undefined;
 
 	if ($page.url.hash != '') {
 		puzzle = generatePuzzleFromBase64Hash($page.url.hash.substring(1));
 	}
-	// else {
-	// 	awaitedPuzzle = generateSudokuPuzzle();
-	// }
-
-	import { page } from '$app/stores';
-
-	console.log($page.url.hash);
 </script>
 
 <svelte:head>
-	<title>Dan's Sudoku (Puzzles generated using third-party library)</title>
+	<title>sudoku (Puzzles generated using third-party library)</title>
 </svelte:head>
 
 <body>
 	<div class="container">
 		<h1>
-			Sudo<s>ku</s>
+			sudo<s>ku</s>
 		</h1>
 		{#if typeof puzzle != 'undefined'}
 			<Puzzle {puzzle} />
